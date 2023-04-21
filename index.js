07493d19ab3d11e03b4a03b83ccc59e0049c2db7
YevsiukoVitaliy <93053290+YevsiukoVitaliy@users.noreply.github.com>
2023-04-21 23:20:47 +0300

    fd

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+let connectedUsers = 0;
+
+io.on('connection', socket => {
+  connectedUsers++;
+  console.log(`A user connected. Total connected users: ${connectedUsers}`);
+});
+
+io.on('connection', socket => {
+  socket.on('disconnect', () => {
+    connectedUsers--;
+    console.log(
+      `A user disconnected. Total connected users: ${connectedUsers}`
+    );
+  });
+});
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});

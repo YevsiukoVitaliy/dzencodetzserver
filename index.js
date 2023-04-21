90bbf7739236dfa7b5bfd1a07edca4c09e71bfdd
YevsiukoVitaliy <93053290+YevsiukoVitaliy@users.noreply.github.com>
2023-04-21 23:26:12 +0300

    sd

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ io.on('connection', socket => {
   });
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });

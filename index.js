7164780c7e012692aa958a46e128b4aeed492ec3
YevsiukoVitaliy <93053290+YevsiukoVitaliy@users.noreply.github.com>
2023-04-24 02:13:31 +0300

    sdds

--- index.js
+++ index.js
@@ -21,5 +21,5 @@ app.use(express.urlencoded({ extended: false }));
 // Use routes
 app.use('/', routes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
